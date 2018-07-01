6cd21b0186c0c302f1f0d1925c297fd8bb5676d7
Jin Yang <yang.youth1020@gmail.com>
2018-07-02 00:03:38 +0800

    Fix list-page

--- mean-project/src/app/app.module.ts
+++ mean-project/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MatToolbarModule } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatTableModule } from '@angular/material/table';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './components/list/list.component';
 import { CreateComponent } from './components/create/create.component';
 import { EditComponent } from './components/edit/edit.component';
+import { IssueService } from './components/issue.service';
 
 
 const routes: Routes = [
@@ -27,9 +33,14 @@ const routes: Routes = [
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(routes),
-    MatToolbarModule
+    MatToolbarModule,
+    HttpClientModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatTableModule
   ],
-  providers: [],
+  providers: [IssueService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- mean-project/src/app/components/list/list.component.ts
+++ mean-project/src/app/components/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IssueService } from '../issue.service';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private isu_service: IssueService) { }
+
+  coldata: string[] = ['title', 'responsible', 'severity', 'status', 'description'];
+
+  mydata:any;
 
   ngOnInit() {
+    this.isu_service.getIssues()
+      .subscribe((issues) => {this.mydata = issues;     
+      });
+  }
+
+  onbtn(id) {
+    console.log('aaa:',id);
   }
 
 }
